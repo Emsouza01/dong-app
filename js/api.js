48209f59bbe46fdab2acb7c6db5ffa3766f74c70
Emsouza01 <63312236+Emsouza01@users.noreply.github.com>
2025-12-16 21:58:42 -0300

    Update Supabase key in api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://tavlmlqmucolsdefkvtl.supabase.co";
-const SUPABASE_KEY = "sb_publishable_NEbHOg9yXKT9eV-Unspsjg_n-8EIoDS";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhdmxtbHFtdWNvbHNkZWZrdnRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDM4MDUsImV4cCI6MjA4MTQ3OTgwNX0.ZJKBb-Evwdb4KnmpIxAkKE88vel-BmSSuu-BcVPuYOM";
 
 async function getShows() {
   const res = await fetch(`${SUPABASE_URL}/rest/v1/shows?select=*`, {
